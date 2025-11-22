6603f8c5fe42e4ebe60ff7b48fe8e28d11a1d380
Md Ikbal <ikbal84336@gmail.com>
2025-11-22 18:53:41 +0530

    08: error tracking

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -3,6 +3,7 @@ import { createOpenAI } from "@ai-sdk/openai";
 import { createAnthropic } from "@ai-sdk/anthropic";
 import { inngest } from "./client";
 import { generateText } from "ai";
+import * as Sentry from "@sentry/nextjs";
 const google = createGoogleGenerativeAI();
 const openai = createOpenAI();
 const antropic = createAnthropic();
@@ -10,14 +11,23 @@ export const execute = inngest.createFunction(
   { id: "execute-ai" },
   { event: "execute/ai" },
   async ({ event, step }) => {
+    Sentry.logger.info("User triggered test log", {
+      log_source: "sentry_test",
+    });
+    console.warn("something is missing");
+    console.error("this is an error i want to track");
     const { steps: geminiSteps } = await step.ai.wrap(
       "gemini-generate-text",
       generateText,
       {
         model: google("gemini-2.0-flash"),
         system: "you are a helpful assistant",
-
         prompt: "what is 2+2",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       }
     );
     const { steps: openaiSteps } = await step.ai.wrap(
@@ -26,8 +36,12 @@ export const execute = inngest.createFunction(
       {
         model: openai("gpt-4"),
         system: "you are a helpful assistant",
-
         prompt: "what is 2+2",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       }
     );
     const { steps: anthropicSteps } = await step.ai.wrap(
@@ -36,8 +50,12 @@ export const execute = inngest.createFunction(
       {
         model: antropic("claude-sonnet-4-5"),
         system: "you are a helpful assistant",
-
         prompt: "what is 2+2",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       }
     );
     return {
